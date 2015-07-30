dd46838303dd0b6577437891f962ba48f195104a
lemon07 <keyson@outlook.com>
2015-07-30 19:28:23 +0800

    “Lemon07”

--- packages.sh
+++ packages.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-dpkg-deb -bZgzip projects/KuaiDial debs
+dpkg-deb -bZgzip projects/ColorAppleBootLogo07 debs
+dpkg-deb -bZgzip projects/ColorAppleBootLogo07iPadHD debs
 dpkg-deb -bZgzip projects/iBlackList debs
+dpkg-deb -bZgzip projects/KuaiDial debs
+dpkg-deb -bZgzip projects/Love07 debs
+dpkg-deb -bZgzip projects/ManchesterUnited07 debs
+dpkg-deb -bZgzip projects/RedAppleBootLogo07 debs
+dpkg-deb -bZgzip projects/Round07 debs
+dpkg-deb -bZgzip projects/Round07iPadHD debs
+dpkg-deb -bZgzip projects/STAR07 debs
+dpkg-deb -bZgzip projects/STARBootLogo07 debs
+dpkg-deb -bZgzip projects/STARWifi07 debs
 # dpkg-deb -bZgzip projects/<project name> <output folder>
